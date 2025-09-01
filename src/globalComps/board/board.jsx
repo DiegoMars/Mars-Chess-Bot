939c8cfab56a5c890914df354ac0b70b58279b74
Diegos_Rad <131321814+DiegoMars@users.noreply.github.com>
2025-08-31 21:49:13 -0700

    Split board into 3 tables

--- src/globalComps/board/board.jsx
+++ src/globalComps/board/board.jsx
@@ -14,28 +14,45 @@ import PlaceholderImg from "./../../assets/pieces/No_image.svg";
 import { useState, useEffect } from "react";
 import styles from "./board.module.css";
 
-const renderRow = (fenElement) => {
+const renderRow = (fenElement, white) => {
   let row = [];
   let fen = fenElement;
   if (Array.isArray(fenElement)){
     fen = fenElement[0];
   }
-  for (let i=0; i < fen.length; i++) {
-    let char = fen.charAt(i);
-    let isNumber = !isNaN(char);
-    if (isNumber) {
-      for (let j = 0; j < char; j++){
-        row.push(" ");
+  if (white) {
+    for (let i=0; i < fen.length; i++) {
+      let char = fen.charAt(i);
+      let isNumber = !isNaN(char);
+      if (isNumber) {
+        for (let j = 0; j < char; j++){
+          row.push(" ");
+        }
+      } else {
+        row.push(char);
       }
-    } else {
-      row.push(char);
     }
+    return row;
+  } else {
+    for (let i=fen.length; i >= 0; i--) {
+      let char = fen.charAt(i);
+      let isNumber = !isNaN(char);
+      if (isNumber) {
+        for (let j = 0; j < char; j++){
+          row.push(" ");
+        }
+      } else {
+        row.push(char);
+      }
+    }
+    return row;
   }
-  return row;
 };
 
 function Board() {
   const [pieces, setPieces] = useState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+  const [moves, setMoves] = useState("8/8/8/8/8/8/8/8");
+  const [white, setWhite] = useState(true);
 
   // Ima try to use FEN to describe the board
   // https://en.wikipedia.org/wiki/Forsyth–Edwards_Notation
@@ -56,26 +73,65 @@ function Board() {
   }
   let fenPositions = pieces.split("/", 8);
   fenPositions[7] = fenPositions[7].split(" ", 1);
+  let fenMoves = moves.split("/", 8);
   let Board = [];
-  for (let i = 0; i < 8; i++) {
-    Board.push(renderRow(fenPositions[i]));
+  let Moves = [];
+  if (white){
+    for (let i = 0; i < 8; i++) {
+      Board.push(renderRow(fenPositions[i], white));
+      Moves.push(renderRow(fenMoves[i], white))
+    }
+  } else {
+    for (let i = 7; i >= 0; i--) {
+      Board.push(renderRow(fenPositions[i], white));
+      Moves.push(renderRow(fenMoves[i], white))
+    }
   }
+  console.log(Moves);
 
   return (
     <section>
-      <table className={styles.board}>
-        <tbody>
-          {Board.map((Row,idx) => (
-            <tr key={idx}>
-              {Row.map((cell,idx) =>(
-                <td key={idx} className={styles.cell}>
-                  <img src={pieceImg[cell]} alt={cell + " piece"} />
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className={styles.boards}>
+        <table className={styles.mainBoard}>
+          <tbody>
+            {Board.map((Row,idx) => (
+              <tr key={idx} className={styles.boardRow}>
+                {Row.map((cell,idx) =>(
+                  <td key={idx} className={styles.cell}>
+                    <img src={PlaceholderImg} alt="placeholder"/>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <table className={styles.moves}>
+          <tbody>
+            {Moves.map((Row,idx) => (
+              <tr key={idx}>
+                {Row.map((cell,idx) =>(
+                  <td key={idx} className={cell == "m" ? styles.move : styles.noMove}>
+                    <img src={PlaceholderImg} alt="placeholder"/>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <table className={styles.pieces}>
+          <tbody>
+            {Board.map((Row,idx) => (
+              <tr key={idx}>
+                {Row.map((cell,idx) =>(
+                  <td key={idx}>
+                    <img src={pieceImg[cell]} alt={cell + " piece"} />
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 }
