219770313376d759e1e1fce3a3a3591840f68d92
Diegos_Rad <131321814+DiegoMars@users.noreply.github.com>
2025-09-09 22:24:01 -0700

    Migrating over to react-chessboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import Board from "./globalComps/board/board.jsx";
 import "./mainStyles/App.css";
-import "./mainStyles/realtimecolors.css";
 
 function App() {
 
