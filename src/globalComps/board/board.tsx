9dde00418568d264e79d247536fcba4053724d6d
Diegos_Rad <131321814+DiegoMars@users.noreply.github.com>
2025-09-16 08:46:34 -0700

    Figuring out dnd/move validation

--- src/globalComps/board/board.tsx
+++ src/globalComps/board/board.tsx
@@ -1,13 +1,31 @@
-import { Chessboard } from "react-chessboard";
+import { Chessboard, type PieceDropHandlerArgs } from "react-chessboard";
 import { Chess } from 'chess.js';
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import styles from "./board.module.css";
 
 function Board() {
+  const gameRef = useRef(new Chess());
+  const chessGame = gameRef.current;
+
+  const [fen, setFen] = useState(chessGame.fen());
+
+  const onPieceDrop = (sourceSquare: string, targetSquare: string) => {
+    const game = chessGame;
+    const move = game.move({ from: sourceSquare, to: targetSquare, promotion: "q" });
+
+    if (move === null) return false;
+
+    setFen(game.fen());
+    console.log("Move made:", move);
+    return true;
+  };
+
   return (
     <section className={styles.boardContainer}>
       <Chessboard
         options={{
+          onPieceDrop: {onPieceDrop},
+          arePiecesDraggable: true,
           darkSquareStyle: { backgroundColor: "var(--secondary)" },
           lightSquareStyle: { backgroundColor: 'var(--primary)' },
         }}
